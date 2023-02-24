11cda6d8c1be3c8523755d8fdfbd9230ca551e05
신선아 <49391145+Se0nAh@users.noreply.github.com>
2023-02-25 06:55:17 +0900

    feat: price tab 추가

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,13 +1,109 @@
 import {PriceData} from "../types";
+import styled from "styled-components";
 
 function Price({tickersData}: {tickersData?: PriceData}) {
   if (!tickersData) return <></>
 
   return (
-  <div>
-    <h1>Price</h1>
+  <PriceContainer>
+    <Item>
+      <div>
+        최고가 ({new Date(tickersData?.quotes?.USD?.ath_date).toLocaleString()})
+      </div>
 
-  </div>);
+      <Value>
+        ${tickersData?.quotes?.USD?.ath_price.toFixed(2)}
+      </Value>
+    </Item>
+    <Item>
+      <div>
+        15분 전보다
+      </div>
+      <Value>
+        {tickersData?.quotes?.USD?.percent_change_15m}%
+      </Value>
+    </Item>
+    <Item>
+      <div>
+        30분 전보다
+      </div>
+      <Value>
+        {tickersData?.quotes?.USD?.percent_change_30m}%
+      </Value>
+    </Item>
+    <Item>
+      <div>
+        1시간 전보다
+      </div>
+      <Value>
+        {tickersData?.quotes?.USD?.percent_change_1h}%
+      </Value>
+    </Item>
+    <Item>
+      <div>
+        6시간 전보다
+      </div>
+      <Value>
+        {tickersData?.quotes?.USD?.percent_change_6h}%
+      </Value>
+    </Item>
+    <Item>
+      <div>
+        12시간 전보다
+      </div>
+      <Value>
+        {tickersData?.quotes?.USD?.percent_change_12h}%
+      </Value>
+    </Item>
+    <Item>
+      <div>
+        12시간 전보다
+      </div>
+      <Value>
+        {tickersData?.quotes?.USD?.percent_change_12h}%
+      </Value>
+    </Item>
+    <Item>
+      <div>
+        24시간 전보다
+      </div>
+      <Value>
+        {tickersData?.quotes?.USD?.percent_change_24h}%
+      </Value>
+    </Item>
+    <Item>
+      <div>
+        7일 전보다
+      </div>
+      <Value>
+        {tickersData?.quotes?.USD?.percent_change_7d}%
+      </Value>
+    </Item>
+
+  </PriceContainer>);
 }
 
 export default Price;
+
+const PriceContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+`
+
+const Bold = styled.div`
+  font-weight: bold;
+`
+
+const Item = styled.li`
+  border-top: 1px solid ${props => props.color};
+  :last-child {
+    border-bottom: 1px solid ${props => props.color};
+  }
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items:  center;
+`
+const Value = styled.div`
+  font-size: 22px;  
+`
